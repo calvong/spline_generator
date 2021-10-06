0761995829c90a3ac8b907e04b489f5572e986c4
calvong <calvongchi@gmail.com>
2021-10-06 12:10:19 +1100

    code clean up

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class PlanReader():
         return pos, vel, accel, traj_time
 
 
-    def interpolate_traj(self, traj, traj_time):
+    def interpolate_traj(self, traj, vel, traj_time):
         """
         :param traj:
         :param traj_time:
@@ -43,57 +43,75 @@ class PlanReader():
         """
 
         joint = 5
+        break_pt = 2
 
-        traj_cs = CubicSpline(traj_time, traj[joint, :], bc_type="natural")
+        pos_cs = CubicSpline(traj_time, traj[joint, :], bc_type="natural")
+        vel_cs = pos_cs.derivative()
+    
+        print "pos coeff:"
+        print pos_cs.c
+        
+        print "vel coeff:"
+        print vel_cs.c
+
+        print "break"
+        print vel_cs.x
 
         xs = np.arange(0, 1, 0.01)
 
-        abc = traj_cs.c.transpose()
+        pos_c = pos_cs.c.transpose()
+        c = pos_c[break_pt, :]
 
-        c = abc[0, :]
+        xp = np.arange(pos_cs.x[break_pt], pos_cs.x[break_pt+1], 0.02, dtype=float)
+        fp = c[0]*np.float_power(xp-vel_cs.x[break_pt], 3) + c[1]*np.float_power(xp-vel_cs.x[break_pt], 2) \
+            + c[2]*(xp-vel_cs.x[break_pt]) + c[3]
 
-        xx = np.arange(0, 0.6, 0.1, dtype=float)
-        f = c[0]*np.float_power(xx, 3) + c[1]*np.float_power(xx, 2) + c[2]*xx + c[3]
+        vel_c = vel_cs.c.transpose()
+        cv = vel_c[break_pt, :]
 
-        c = abc[1, :]
+        xv = np.arange(vel_cs.x[break_pt], vel_cs.x[break_pt+1], 0.02, dtype=float)
+        fv = cv[0]*np.float_power(xv-vel_cs.x[break_pt], 2) + cv[1]*np.float_power(xv-vel_cs.x[break_pt], 1) + cv[2] - vel_c[0, 2]
 
-        xx2 = np.arange(0.3, 0.6, 0.1, dtype=float)
-        f2 = c[0]*np.float_power(xx2-traj_cs.x[1], 3) + c[1]*np.float_power(xx2-traj_cs.x[1], 2) + c[2]*(xx2-traj_cs.x[1]) + c[3]
+        # c_reshape = np.reshape(pos_cs.c.transpose(), (pos_cs.c.shape[0]*pos_cs.c.shape[1]))
 
-        c_reshape = np.reshape(traj_cs.c.transpose(), (traj_cs.c.shape[0]*traj_cs.c.shape[1]))
-
-        traj_cs.d
-
-        # plt.plot(traj_time, traj[1,:])
-        #plt.plot(xs, traj_cs(xs), '.')
-        plt.plot(traj_time, traj[joint, :], 'o')
-        plt.plot(xx, f ,'x')
-        plt.plot(xx2, f2, 'x')
+        plt.figure(1)
+        plt.plot(xs, pos_cs(xs), '.r')
+        plt.plot(traj_time, traj[joint, :], '*k')
+        plt.plot(xp, fp, 'og')
         plt.grid()
-        plt.show()
+        plt.title("position")
 
+        plt.figure(2)
+        plt.plot(xs, vel_cs(xs), '.r')
+        plt.plot(traj_time, vel[joint, :], '*k')
+        plt.plot(xv, fv, 'og')
+        plt.grid()
+        plt.title("velocity")
+
+        plt.show()
 
 
 def run():
     print "hello world"
 
-    # p = PlanReader()
-    #
-    # pos, vel, accel, traj_time = p.read_plan_file("plan_joint6.txt")
-    #
-    # p.interpolate_traj(pos, traj_time)
+    p = PlanReader()
 
-    r = R.from_quat([0, 0, 0, 1])
-    rxyz = R.from_euler('xyz', [0, 90, 0], degrees=True)
-    rx = R.from_euler('xyz', [0, 0, 45], degrees=True)
-    new_r = R.from_dcm(np.matmul(rxyz.as_dcm(), rx.as_dcm()))
+    pos, vel, accel, traj_time = p.read_plan_file("plan_joint6.txt")
 
-    print rxyz.as_quat()
+    p.interpolate_traj(pos, vel, traj_time)
 
-    print new_r.as_quat()
+    # r = R.from_quat([0, 0, 0, 1])
+    # rxyz = R.from_euler('xyz', [0, 270, 0], degrees=True)
+    # rx = R.from_euler('xyz', [0, 0, 45], degrees=True)
+    # new_r = R.from_dcm(np.matmul(rxyz.as_dcm(), rx.as_dcm()))
 
-    print new_r.as_dcm()
-    
+    # print rxyz.as_quat()
+
+    # print new_r.as_quat()
+
+    # print new_r.as_dcm()
+
 if __name__ == '__main__':
     run()
+
 
